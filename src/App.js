14a443c335de6e2035706d9b82e0b33f229d414c
gabolep <gabriel.escudero@alumnos.ucn.cl>
2022-02-09 21:50:28 -0300

    infinite pagination

--- src/App.js
+++ src/App.js
@@ -8,15 +8,23 @@ import Select from './components/Select';
 import News from './components/News';
 import Pages from './components/Pages';
 
-
-
-
-
-
-
-
-class App extends Component {
-  componentDidMount(){
+    //setting localstorage
+    let type = localStorage.getItem('type');
+    if(!type){
+        type = localStorage.setItem('type','all');
+    }
+    let angular = localStorage.getItem('angular');
+    let react = localStorage.getItem('react');
+    let vuejs = localStorage.getItem('vuejs');
+    if(!angular){
+        angular = localStorage.setItem('angular','false');
+    }
+    if(!react){
+        react = localStorage.setItem('react','false');
+    }
+    if(!vuejs){
+        vuejs = localStorage.setItem('vuejs','false');
+    }
     let page = localStorage.getItem('page');
     if(!page){
         page = localStorage.setItem('page','0');
@@ -25,7 +33,10 @@ class App extends Component {
     if(!favNews){
         favNews = localStorage.setItem('favNews','');
     }
-}
+
+
+class App extends Component {
+  
   render(){
     return (
       <div className="App">
@@ -33,7 +44,7 @@ class App extends Component {
           <Buttons/>
           <Select/>
           <News/>
-          <Pages></Pages>     
+          <Pages></Pages>
       </div>
     );
   }
--- src/components/Page.js
+++ src/components/Page.js
@@ -13,7 +13,6 @@ class Page extends Component{
             page.toString();
             const realPage = localStorage.getItem('page');
             if(realPage === '0'){
-                console.log('hola');
                 return;
             }else{
                 localStorage.setItem('page', page); 
@@ -37,15 +36,24 @@ class Page extends Component{
         let page = localStorage.getItem('page');
         let pageInt = parseInt(page);
         pageInt = pageInt + 1;
+        const pageplus = pageInt;
         const pageText = pageInt.toString();
         const textBox = p.page
-        if(textBox === pageText){
+        parseInt(textBox);
+        if(p.page === pageText){
             return(
                 <div className='cardSelected'> 
-                {p.page}
+                {pageText}
                 </div> 
             )
-        }else{
+        }else if(pageplus > 9 && p.page == 9){
+            return(
+                <div className='cardSelected'> 
+                {pageText}
+                </div> 
+            )
+        }
+        else{
             return(
                 <div className='card' onClick={(e,p) => this.changePage(e,p)}> 
                     {p.page}
